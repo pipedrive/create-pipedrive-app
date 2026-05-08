9800ce93481bb85a4f71dd041b257982f2cfbe04
Denis <deniss.dmitrijevs@pipedrive.com>
2026-05-08 13:45:26 +0300

    feat: add appExtensions stub generator

---
+++ src/generators/node/appExtensions.ts
@@ -0,0 +1,19 @@
+import { join } from 'path';
+import { writeFile } from '../../utils/writeFile.js';
+import type { GeneratorOptions } from '../interface.js';
+
+const routerStub = `import { Router } from 'express';
+export default Router();
+`;
+
+export async function generateAppExtensions(
+  outputDir: string,
+  options: GeneratorOptions,
+): Promise<void> {
+  if (options.appExtensions.includes('custom-panel')) {
+    await writeFile(join(outputDir, 'src/app-extensions/panel/index.ts'), routerStub);
+  }
+  if (options.appExtensions.includes('custom-modal')) {
+    await writeFile(join(outputDir, 'src/app-extensions/modal/index.ts'), routerStub);
+  }
+}
