828008641005649c181cf59480d5f49cb37f2b66
Youssef <youssef.saber@pipedrive.com>
2026-05-11 14:11:16 +0300

    AINATIVEM-44 refactor oauth generator to use templates

--- src/generators/node/oauth.ts
+++ src/generators/node/oauth.ts
@@ -1,14 +1,8 @@
-import dedent from 'dedent';
 import { join } from 'path';
 import { writeFile } from '../../utils/writeFile.js';
 import type { GeneratorOptions } from '../interface.js';
+import { expressRouterFile } from '../../utils/templates.js';
 
 export async function generateOauth(outputDir: string, _options: GeneratorOptions): Promise<void> {
-	await writeFile(
-		join(outputDir, 'src/oauth/index.ts'),
-		dedent`
-      import { Router } from 'express';
-      export default Router();
-    `,
-	);
+	await writeFile(join(outputDir, 'src/oauth/index.ts'), expressRouterFile());
 }
